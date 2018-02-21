7f53bf3f8c6a2074230d7d895337710a74d5c28d
carlos arosemena <ceag68@hotmail.com>
2018-02-21 15:12:28 -0500

    new chage

--- models/store.py
+++ models/store.py
@@ -18,11 +18,9 @@ class StoreModel(db.Model):
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
 
-
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
-
 
     def delete_from_db(self):
         db.session.delete(self)
